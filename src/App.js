af96b83dd86d1ed4bf723dfab2dee825febf3ab5
Marco Cobian <maca9608@gmail.com>
2021-03-15 14:28:01 -0600

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import io from 'socket.io-client';
+class App extends Component{
+    constructor(props) {
+        super(props);
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentWillUnmount() {
+        this.socket.close()
+    }
+
+    componentDidMount(){
+        this.socket = io.connect("http://localhost:5000", {
+            reconnection: true,
+        })
+        console.log('se conecta');
+        this.socket.on("responseMessage", message => {
+            console.log("este es el mensaje-->",message);
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>
+                    Cliente socket
+                </h1>
+            </div>
+        )
+    }
 }
 
 export default App;
